6d128d88f14909b7675f1e6fa2458d91b554e17f
Timioe <55991668+Timioe@users.noreply.github.com>
2019-11-05 09:23:17 +0100

    05-11-923
    Verusch NavTabs

--- public/page-detail/page-detail.js
+++ public/page-detail/page-detail.js
@@ -39,8 +39,46 @@ class PageDetail {
             return;
         }
 
-        //document.getElementByClassName("nav-item").addEventListener("click", tabListener);
+        window.addEventListener("load", () => {
+            // Funktion zum Umschalten der Tabreiter
+            let switchTabPage = (clickedTabItem) => {
+                // Erst mal alle Tabseiten ausblenden
+                clickedTabItem.parentNode.childNodes.forEach(tabItem => {
+                    if (tabItem.nodeType != Node.ELEMENT_NODE) return;
 
+                    tabItem.classList.remove("active");
+                    let tabContent = document.querySelector(tabItem.dataset.tabContent);
+
+                    if (tabContent != null) {
+                        tabContent.classList.add("tab-page");
+                    }
+                });
+
+                // Dann die ausgewählte Tabseite anzeigen
+                clickedTabItem.classList.add("active");
+                let tabContent = document.querySelector(clickedTabItem.dataset.tabContent);
+
+                if (tabContent != null) {
+                    tabContent.classList.remove("tab-page");
+                }
+            }
+
+            // Alle Tablaschen in der Seite suchen
+            let tabItems = document.querySelectorAll(".tab-item");
+
+            // Event Handler registrieren und erste Tabseite anzeigen
+            tabItems.forEach(tabItem => {
+                // Bist du eine aktive Tablasche? Dann Inhalt anzeigen.
+                if (tabItem.classList.contains("active")) {
+                    switchTabPage(tabItem);
+                }
+
+                // Aktive Seite bei Klick auf die Lasche wechseln
+                tabItem.addEventListener("click", event => {
+                    switchTabPage(event.target);
+                });
+            });
+        });
 
         try {
         // Seite zur Anzeige bringen
